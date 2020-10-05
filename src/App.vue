4ee5559a1b389c1957f74bdda9147c0968df3aec
Vips120 <vipulsingh0000@gmail.com>
2020-10-05 15:51:40 +0530

    vue upvoting first project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div class="container fluid">
+      <div class="row" v-for="user in user" v-bind:key="user.id" style="margin:10px">
+<div class="col-md-3">
+  <img 
+   v-bind:src="user.thumbnailUrl"
+  />
+</div>
+<div class="col-md-9 text-left">
+<a v-bind:href="user.url">
+  <span> 
+    {{user.title}}
+  </span>
+  </a><span>#{{user.id}}</span>
+  <span class="pull-right">
+  <i class="fa fa-chevron-circle-up f-2x" aria-hidden="true"></i> 16
+  </span>
+  <span>
+    {{user.username}}
+  </span>
+</div>
+</div>
+    </div>
+</template>
+<script>
+import users from "../users";
+export default {
+  el: '#app',
+  data: () => {
+    return {user: users}
+  }
+}
+
+</script>
