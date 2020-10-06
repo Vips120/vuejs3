943c6f2c693874c6d6a7567f32c8d216080edaef
Vips120 <vipulsingh0000@gmail.com>
2020-10-06 16:19:09 +0530

    vuejs3 components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 
 <template>
-  <div class="container fluid">
-      <div class="row" v-for="user in sortedInfo" v-bind:key="user.id" style="margin:10px">
-<div class="col-md-3">
-  <img 
-   v-bind:src="user.thumbnailUrl"
-  />
-</div>
-<div class="col-md-9 text-left">
-<a v-bind:href="user.url">
-  <span> 
-    {{user.title}}
-  </span>
-  </a><span>#{{user.id}}</span>
-  <span class="pull-right">
-  <i class="fa fa-chevron-circle-up f-2x" aria-hidden="true"
-  v-on:click="upvote(user.id)"
-  ></i> {{user.votes}}
-  </span>
-  <span>
-    {{user.username}}
-  </span>
-</div>
-</div>
-    </div>
+<SubmissionComponent
+ v-bind:user="user"
+ v-bind:sortedinfo="sortedInfo"
+></SubmissionComponent>
 </template>
 <script>
 import users from "../users";
+import SubmissionComponent from "./components/submission-component.vue";
 export default {
   el: '#app',
   data: () => {
@@ -37,11 +18,14 @@ export default {
       return this.user.sort((a,b) => b.votes - a.votes )
     }
   },
-  methods:{
-    upvote(voteId){
-      const data = this.user.find(item => item.id === voteId);
-      data.votes++;
-    }
+  // methods:{
+  //   upvote(voteId){
+  //     const data = this.user.find(item => item.id === voteId);
+  //     data.votes++;
+  //   }
+  // },
+  components:{
+    SubmissionComponent
   }
 }
 
---
+++ src/components/submission-component.vue
@@ -0,0 +1,41 @@
+<template>
+      <div class="container fluid">
+      <div class="row border"
+       v-bind:class="{'border-danger': user.votes >= 20}"
+       v-for="user in sortedinfo" v-bind:key="user.id" style="margin:10px">
+<div class="col-md-3">
+  <img 
+   v-bind:src="user.thumbnailUrl"
+  />
+</div>
+<div class="col-md-9 text-left">
+<a v-bind:href="user.url">
+  <span> 
+    {{user.title}}
+  </span>
+  </a><span>#{{user.id}}</span>
+  <span class="pull-right">
+  <i class="fa fa-chevron-circle-up f-2x" aria-hidden="true"
+  v-on:click="upvote(user.id)"
+  ></i> {{user.votes}}
+  </span>
+  <span>
+    {{user.username}}
+  </span>
+</div>
+</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SubmissionComponent',
+      methods:{
+    upvote(voteId){
+      const data = this.user.find(item => item.id === voteId);
+      data.votes++;
+    }
+  },
+    props:['user','sortedinfo']
+}
+</script>
